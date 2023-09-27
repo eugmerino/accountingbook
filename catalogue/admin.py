7358cb1679a5e73183fb26724c6802968b6ba39f
Eugenio Merino <merinoeug118@gmail.com>
2023-09-26 21:24:18 -0600

    Add admin configurations

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,4 +1,28 @@
 from django.contrib import admin
 from catalogue.models import Account
 
-admin.site.register(Account)
+
+#-------- Admin site --------
+admin.site.index_title = "Accounting Book"
+admin.site.site_header = "Administración"
+admin.site.site_title = "Accounting Book"
+# ---------------------------
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'code',
+        'account_r',
+    )
+    fields = (
+        'code',
+        'name',
+        'description',
+        'parent',
+        'account_r',
+    )
+    search_fields = ('name', 'code')
+    autocomplete_fields = ('parent',)
+    list_per_page = 10
+    readonly_fields = ('code',)
