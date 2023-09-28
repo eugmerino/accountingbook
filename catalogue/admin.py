5731295d8a4853eb284e354b668fe8f88eb749ad
Tito Merino <33304055+titomerino@users.noreply.github.com>
2023-09-28 13:33:52 -0600

    Catalogue: Add admin configurations

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,20 +1,24 @@
 from django.contrib import admin
+
 from catalogue.models import Account
 
 
 #-------- Admin site --------
 admin.site.index_title = "Accounting Book"
-admin.site.site_header = "Administración"
+admin.site.site_header = "Accounting Book"
 admin.site.site_title = "Accounting Book"
 # ---------------------------
+
 
 @admin.register(Account)
 class AccountAdmin(admin.ModelAdmin):
     list_display = (
-        'name',
         'code',
+        'name',
+        'description',
         'account_r',
     )
+    list_display_links = ['name']
     fields = (
         'code',
         'name',
@@ -25,4 +29,13 @@ class AccountAdmin(admin.ModelAdmin):
     search_fields = ('name', 'code')
     autocomplete_fields = ('parent',)
     list_per_page = 10
-    readonly_fields = ('code',)
+    ordering = ['code']
+
+    def get_readonly_fields(self, request, obj=None):
+        """
+        Retorna los campos de la cuenta que no se podrán editar
+        """
+        fields = ['code']
+        if obj:
+            fields.append('parent')
+        return fields
