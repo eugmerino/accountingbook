196c2f1c85ca496432d524c05eb59df3a9598813
Tito Merino <33304055+titomerino@users.noreply.github.com>
2023-09-28 11:19:35 -0600

    Catalogue: Add signals

--- catalogue/models.py
+++ catalogue/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# import signals (Disparadores)
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
 
 class Account(models.Model):
     """
@@ -26,3 +30,21 @@ class Account(models.Model):
     class Meta:
          verbose_name="Cuenta"
          verbose_name_plural="Cuentas"
+
+
+# Signals
+@receiver(pre_save, sender=Account)
+def account_pre_save_receiver(sender, instance, *args, **kwargs):
+    """
+    Se encarga de asignar el código de la cuenta.
+    """
+    if not instance.id:
+        if not instance.parent:
+            # Devuelve el numero de cuentas padre registradas.
+            last_id = Account.objects.filter(parent=None).count()
+            instance.code=str(last_id+1)
+        else:
+            # Devuelve el numero de cuentas hijas que tiene la cuenta padre
+            last_id = Account.objects.filter(parent=instance.parent_id).count()
+            instance.code=instance.parent.code + str(last_id+1)
+            
