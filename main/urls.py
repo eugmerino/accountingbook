4c4f33e31d2daab8e54d29d76f24b934fbbc8e86
AlejandroFlores004 <alejandroflores004@outlook.com>
2024-01-07 18:51:41 -0600

    Login y logout correctos

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.signIn),
-    path('dashboard',views.dashBoard, name='dashboard')
+    path('',views.start),
+    path('login/',views.signIn, name="login"),
+    path('logout/',views.singOut, name="logout"),
+    path('dashboard/',views.dashBoard, name='dashboard')
 ]
