06bf7ad2945f44acd3e9a43a29532d202b6aa6b4
Ian21023 <pm21023@ues.edu.sv>
2024-12-10 13:23:49 -0600

    naturaleza de cuentas casi-terminada

--- catalogue/forms.py
+++ catalogue/forms.py
@@ -1,14 +1,35 @@
 from django import forms
 from .models import Account
+from .models import Balance_type
 
 class accntForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['name', 'description', 'parent','account_r']
         widgets = {
-            'name': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.TextInput(attrs={'class': 'form-control'}),
-            'parent': forms.Select(attrs={'class': 'form-select'}),
-            'account_r': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+            'name': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'description': forms.TextInput(attrs={'class': 'form-control mb-3'}),
+            'parent': forms.Select(attrs={'class': 'form-select mb-3'}),
+            'account_r': forms.CheckboxInput(attrs={'class': 'form-check-input mb-3'}),
 
         }
+
+class natureForm(forms.ModelForm):
+    class Meta:
+        model = Balance_type
+        fields = ['main_account', 'nature_of_balance']
+        widgets = {
+            'main_account': forms.Select(attrs={'class': 'form-select mb-3'}),
+            'nature_of_balance': forms.Select(
+            choices=[(False, 'Deudor'), (True, 'Acreedor')],
+            attrs={'class': 'form-select'}
+        ),
+        }
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Excluir las cuentas ya presentes en Balance_type
+        assigned_accounts = Balance_type.objects.values_list('main_account', flat=True)
+        self.fields['main_account'].queryset = Account.objects.filter(
+            parent=None
+        ).exclude(id__in=assigned_accounts)
--- catalogue/views.py
+++ catalogue/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Account
+from .models import Balance_type
 from .forms import accntForm
+from .forms import natureForm
 from django.contrib import messages
 
 # Create your views here.
@@ -20,14 +22,39 @@ def createAccntView(request):
     if request.method == 'POST':
         accntForm2 = accntForm(request.POST)
 
-        if accntForm2.is_valid():
-            
-            item = accntForm2.save()
+        item = accntForm2.save()
 
+        if accntForm2.is_valid():
             messages.success(request, "La cuenta se ha guardado exitosamente.")
             return redirect('accounts')  
         else:
             messages.error(request, "El error fue empezar la carrera.")
 
-    objects={"accntForm":accntForm2}
-    return render(request,'formAccnt.html',objects)
+    objects1={"accntForm":accntForm2}
+    return render(request,'formAccnt.html',objects1)
+
+@login_required(login_url="/login")
+def natureView(request):
+    natures = Balance_type.objects.all().order_by("main_account")
+
+    return render(request,'natures.html',{
+        'natures':natures
+    })
+
+@login_required(login_url="/login")
+def createNaturView(request):
+    natureForm2=natureForm()
+
+    if request.method == 'POST':
+        natureForm2 = natureForm(request.POST)
+
+        item2 = natureForm2.save()
+
+        if natureForm2.is_valid():
+            messages.success(request, "La cuenta se ha guardado exitosamente.")
+            return redirect('natures')  
+        else:
+            messages.error(request, "El error fue empezar la carrera.")
+
+    objects2={"natureForm":natureForm}
+    return render(request,'formNatur.html',objects2)
